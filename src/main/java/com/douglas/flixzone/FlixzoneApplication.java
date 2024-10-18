f1cce0319ba1a0838e2bb938a0020f6af344b79a
DouglasIde <douglasymide@gmail.com>
2024-10-18 15:26:23 -0300

    first commit

---
+++ src/main/java/com/douglas/flixzone/FlixzoneApplication.java
@@ -0,0 +1,13 @@
+package com.douglas.flixzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlixzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlixzoneApplication.class, args);
+	}
+
+}
