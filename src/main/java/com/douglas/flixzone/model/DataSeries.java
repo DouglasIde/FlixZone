2048256b5efb5fa45606914028481e25c0e2ba2f
DouglasIde <douglasymide@gmail.com>
2024-10-18 15:39:52 -0300

    Feat: Creating the series model class

---
+++ src/main/java/com/douglas/flixzone/model/DataSeries.java
@@ -0,0 +1,10 @@
+package com.douglas.flixzone.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DataSeries(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer temporadas,
+                         @JsonAlias("imdbRating") String avaliacao){
+}
