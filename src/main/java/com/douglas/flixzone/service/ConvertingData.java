1821ff28b763ff3a60c2faccd733d8359d764baf
DouglasIde <douglasymide@gmail.com>
2024-10-21 08:10:20 -0300

    Feat: Creating and importing the interface ICovertData

--- src/main/java/com/douglas/flixzone/service/ConvertingData.java
+++ src/main/java/com/douglas/flixzone/service/ConvertingData.java
@@ -1,5 +1,6 @@
 package com.douglas.flixzone.service;
 
+import com.douglas.flixzone.repository.IConvertData;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
