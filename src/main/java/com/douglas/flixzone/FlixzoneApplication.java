993789560abf5b2a35da359742c4921b540af824
DouglasIde <douglasymide@gmail.com>
2024-10-25 08:55:26 -0300

    Feat: Configuring the API server

--- src/main/java/com/douglas/flixzone/FlixzoneApplication.java
+++ src/main/java/com/douglas/flixzone/FlixzoneApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FlixzoneApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FlixzoneApplication.class, args);
 	}
 
